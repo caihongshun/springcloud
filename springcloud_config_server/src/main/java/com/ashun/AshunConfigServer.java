d7867b22c1600831dd7d25268b78b16b1c29cf79
caihongshun <caihongshun>
2019-04-12 15:31:42 +0800

    更新配置文件

--- springcloud_config_server/src/main/java/com/ashun/AshunConfigServer.java
+++ springcloud_config_server/src/main/java/com/ashun/AshunConfigServer.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
-@RefreshScope
 public class AshunConfigServer {
 
     public static void main(String[] args) {
